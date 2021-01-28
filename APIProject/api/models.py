84285450e890fcbead55d650b2b5ea9d9def8adf
Paul Okello <paulokello273@gmail.com>
2021-01-28 19:03:01 +0300

    Import debugging👧👧

--- APIProject/api/models.py
+++ APIProject/api/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
 class Article(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     description = models.TextField()
+    
     def __str__(self):
         return self.title
 
--- APIProject/api/views.py
+++ APIProject/api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Article
+from django.shortcuts import render,HttpResponse
+from . import models
 from .serializers import ArticleSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
@@ -8,7 +8,7 @@ from rest_framework.parsers import JSONParser
 def article_list(request):
     # get all articles 
     if request.method == "GET":
-        articles = Article.objects.all()
+        articles = models.Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
         return JsonResponse(serializer
         .data, safe=False
@@ -22,3 +22,29 @@ def article_list(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer
         .errors, status=400)
+        
+def article_details(request, pk):
+    try:
+        article = models.Article.objects.get(pk=pk)
+        
+    except models.Article.DoesNotExist :
+        return HttpResponse(status=404)
+    
+    if request.method == "GET":
+        serializer = ArticleSerializer(article
+        )
+        return JsonResponse(serializer.data)
+    
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(data=data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer
+        .errors, status=400)
+    elif request.method == "DELETE":
+        article.delete()
+        return HttpResponse(status=204)
+            
