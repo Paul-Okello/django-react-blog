f15f865574edf4302a6b4420f2f3b2ae9b7109ce
Paul Okello <paulokello273@gmail.com>
2021-01-31 15:45:52 +0300

    urls with class based views🚖🚖

--- APIProject/api/urls.py
+++ APIProject/api/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('articles/', views.article_list
-    ),
-    path('articles/<int:pk>/', views.article_details),
-    
+    path("articles/", views.ArticleList.as_view()),
+    path("articles/<int:id>/", views.ArticleDetails.as_view()),
 ]
+
